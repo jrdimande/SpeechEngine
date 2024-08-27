1144dd6babf44bf32e51e7f88d5ed66e9a87d8fe
jrdimande <timoteojrdimande@gmail.com>
2024-08-27 23:06:50 +0200

    ...

---
+++ Assistant.py
@@ -0,0 +1,27 @@
+import pyttsx3
+import speech_recognition as sr
+
+
+#Speak
+def speak(audio):
+    speak_txt = pyttsx3.init()
+    speak_txt.say(audio)
+    speak_txt.runAndWait()
+
+#Hear
+def mic():
+    recognize = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        recognize.pause_threshold = 1
+        audio = recognize.listen(source)
+
+    try:
+        print("Recognizing....")
+        command = recognize.recognize_google(audio, language = 'En')
+        print(command)
+    except Exception as e:
+        print(e)
+
+
+
